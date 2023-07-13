0e0b16d5a9558d01dc2c52220e04ef3d34e9445c
Kibigo <jacksonkibigo99@gmail.com>
2023-07-13 23:51:54 +0300

    First commit

--- index.js
+++ index.js
@@ -1,29 +1,84 @@
-const oils = document.getElementsByName('oils');
+//declaring the prices of commidities
+
+const premiumPrice = 2000;
+const specialPrice = 1500;
+const regularPrice = 1000;
+
+const vat = 0.16;
+
+const shippingDetails = {
+    nairobi: "FREE",
+    mombasa: 600,
+    nakuru:500,
+    kisumu:400,
+    kiambu:300
+
+}
+
+shippingLocation = document.getElementById("location");
+
+var selectedLocation = shippingLocation.value;
+
+var shippingMoney;
+
+shippingLocation.addEventListener('change', function(){
+    selectedLocation = this.value;
+
+    if (selectedLocation === "nairobi"){
+        shippingMoney = shippingDetails[selectedLocation];
+    }else if (selectedLocation === "mombasa"){
+        shippingMoney = shippingDetails[selectedLocation];
+    }else if (selectedLocation === "nakuru"){
+        shippingMoney = shippingDetails[selectedLocation];
+    }else if (selectedLocation === "kisumu"){
+        shippingMoney = shippingDetails[selectedLocation];
+    }else if (selectedLocation === "kiambu"){
+        shippingMoney = shippingDetails[selectedLocation];
+    }
+    
+})
 
 
-oils.forEach(function(oil){
-    oil.addEventListener('change', finalAmont);
-});
+function calculateFinalAmount (e){
+    e.preventDefault();
+    //solving for the number of bottles entered by the user
 
-function finalAmont(){
-    const tax = 0.16;
-    var total = '';
-    var totalTax = '';
-    var num = document.getElementById('numb').value;
-
-    oils.forEach(function(oil){
-        if (oil.checked){
-            totalTax += parseInt(oil.value*tax)
-            total += (parseInt(oil.value) + parseInt(totalTax)) *num;
-        }
-    });
-
+    var bottleInput = document.getElementById("bottle").value;
     
 
-    const submit = document.getElementById('submit');
-    submit.addEventListener('click', function(e){
-        e.preventDefault();
-        document.getElementById('totalTax').textContent = totalTax;
-        document.getElementById('total').textContent = total;
-    })
+    //solving for tax of commodities bought 
+    var selectedCommodity = document.querySelector('input[type="radio"]:checked').value;
+
+    var totalTax;
+    
+    if (selectedCommodity === "premium"){
+        totalTax = (premiumPrice*bottleInput)*vat;
+    }else if (selectedCommodity === "special"){
+        totalTax = (specialPrice*bottleInput)*vat;
+    }else if (selectedCommodity === "regular"){
+        totalTax = (regularPrice*bottleInput)*vat;
+    }
+    
+
+    //solving for total amount
+
+    var totalAmount;
+
+    if (selectedCommodity === "premium"){
+        totalAmount = (premiumPrice*bottleInput)+totalTax+shippingMoney;
+    }else if (selectedCommodity === "special"){
+        totalAmount = (specialPrice*bottleInput)+totalTax+shippingMoney;
+    }else if(selectedCommodity){
+        totalAmount = (regularPrice*bottleInput)+totalTax+shippingMoney;
+    }
+    document.getElementById("totalTax").textContent = totalTax;
+    document.getElementById("shipping").textContent = shippingMoney;
+    document.getElementById("total").textContent = totalAmount;
+
 }
+
+const submitButton = document.getElementById("buy");
+submitButton.addEventListener("click", calculateFinalAmount);
+
+
+
